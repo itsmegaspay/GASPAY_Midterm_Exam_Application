89ece656c7a95c00eef9f6f95f546652ac0d55ac
itsmegaspay <erwingaspay0623@gmail.com>
2026-02-20 23:28:37 +0800

    feat: Created ProductController with anime products array and defined routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+```
+
+---
+
 });
